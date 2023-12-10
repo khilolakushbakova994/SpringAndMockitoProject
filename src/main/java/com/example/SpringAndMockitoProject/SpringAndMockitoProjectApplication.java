0b590f527cc79591026cbc41807d4579d4a5ba29
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-12-10 12:39:05 +0200

    Initial commit

---
+++ src/main/java/com/example/SpringAndMockitoProject/SpringAndMockitoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAndMockitoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMockitoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMockitoProjectApplication.class, args);
+	}
+
+}
